c0782c10c17d80063289c32b3665fcbdc9cd473a
prem-12345 <premkpatil1997@gmail.com>
2021-06-04 19:21:53 +0530

    second commit

--- arrith.sh
+++ arrith.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 #Day 10: Arithmetic Computation & Sorting
+
+#UC_01: Take three inputs
+
+echo "Enter the number:"
+read a
+
+echo "Enter the number:"
+read b
+
+echo "Enter the number:"
+read c
